293f2fc0d23e99772267283ec0c9b9b378efb5ea
Sara Withrow <saralouise1107@gmail.com>
2023-04-02 13:27:36 -0400

    changes saved

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let counter = 1;
+let random = counter ++;
+
+let num3 = 20;
+let num4 = 5;
+const mod = num3 / num4;
+
+
+let max = Math.max(1, 3, 6, 20, 8)
+
+
